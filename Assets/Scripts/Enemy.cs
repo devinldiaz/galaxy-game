ff0ab5ecff368293dae7ca896f9980e6c906d717
Devin <devinlainediaz@gmail.com>
2025-11-29 17:03:19 -0500

    instantiate explosion

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,8 +2,11 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] ParticleSystem explosion;
     void OnParticleCollision(GameObject other)
     {
+        Instantiate(explosion, this.transform.position, Quaternion.identity);
+
         Destroy(this.gameObject);
     }
 }
